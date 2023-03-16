a492fd34e8386a5bd27f368a4ca01d97590f46f6
MrGoblings <emomomchev@gmail.com>
2023-03-16 23:27:03 +0200

    some binary tree and some vector fixes

--- binary_tree/binary_tree.h
+++ binary_tree/binary_tree.h
@@ -1,5 +1,6 @@
 typedef struct binary_tree_t {
     void* data;
+    int priority;
 
     struct binary_tree_t* left;
     struct binary_tree_t* right;
@@ -9,10 +10,9 @@ typedef struct binary_tree_t {
 
 Binary_tree* binary_tree_init();
 unsigned int binary_tree_size(Binary_tree* q);
-void binary_tree_push(Binary_tree* q, void* value);
+void binary_tree_push(Binary_tree* q, void* value, int priority);
 void* binary_tree_pop(Binary_tree* q);
 int binary_tree_contains(Binary_tree* q, void* value);
-void* binary_tree_at(Binary_tree* q, unsigned int index);
 void binary_tree_clear(Binary_tree* q);
 void binary_tree_destroy(Binary_tree* q);
 void binary_tree_print(Binary_tree* q);
